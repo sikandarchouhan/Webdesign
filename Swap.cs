6be40d19fad8f02ef4a0cfebc4e207a6455ab584
sikandarkrchouhan <sikandarkrchouhan@gmail.com>
2023-12-23 04:45:07 +0000

    Swap file  is updated

--- Swap.cs
+++ Swap.cs
@@ -4,7 +4,11 @@ public class Swap
 {
     public static void Main(string[] args)
     {
-        int a=5, b=10;
+        int a, b;
+        Console.Write("Input a = ");
+        a=int.Parse(Console.ReadLine());
+        Console.Write("Input b = ");
+        b=int.Parse(Console.ReadLine());
         Console.WriteLine("Before swap a = "+a+" and b ="+b);
         a=a*b;
         b=a/b;
